c3f8d09ad569d7364e9e27bce84a75fb1b3a6653
Jordan S <jpstanway@gmail.com>
2022-02-10 20:18:39 -0800

    cover remaining cases and complete challenge

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,32 +1,33 @@
 import numpy as np
 
-
 def calcMVS(func):
-    axis1 = func(0).tolist()
-    axis2 = func(1).tolist()
-    flattened = func().flatten().tolist()[0]
+  axis1 = func(0).tolist()
+  axis2 = func(1).tolist()
+  flattened = func().flatten().tolist()[0]
 
-    return [axis1, axis2, flattened]
-
+  return [axis1, axis2, flattened]
 
 def calcMetric(matrix, func):
-    axis1 = func(matrix, axis=0).tolist()
-    axis2 = func(matrix, axis=1).tolist()
-    flattened = func(matrix)
+  axis1 = func(matrix, axis=0).tolist()
+  axis2 = func(matrix, axis=1).tolist()
+  flattened = func(matrix)
 
-    return [axis1, axis2, flattened]
-
+  return [axis1, axis2, flattened]
 
 def calculate(list):
-    dict = {}
-    arr = np.array(list)
-    matrix = np.reshape(arr, (3, 3))
-    dict['mean'] = calcMVS(matrix.mean)
-    dict['variance'] = calcMVS(matrix.var)
-    dict['standard-deviation'] = calcMVS(matrix.std)
-    dict['max'] = calcMetric(matrix, np.amax)
-    dict['min'] = calcMetric(matrix, np.amin)
-    dict['sum'] = calcMetric(matrix, np.sum)
+  dict = {}
 
-    print(dict)
-    return dict
+  if (len(list) < 9):
+    raise ValueError('List must contain nine numbers.') 
+
+  arr = np.array(list)
+  matrix = np.reshape(arr, (3, 3))
+
+  dict['mean'] = calcMVS(matrix.mean)
+  dict['variance'] = calcMVS(matrix.var)
+  dict['standard deviation'] = calcMVS(matrix.std)
+  dict['max'] = calcMetric(matrix, np.amax)
+  dict['min'] = calcMetric(matrix, np.amin)
+  dict['sum'] = calcMetric(matrix, np.sum)
+
+  return dict
